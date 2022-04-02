8b04f538dc799b284652e89781446fd19bacc720
Enrico Grillini <enrico.grillini@gmail.com>
2022-04-02 13:29:11 +0200

    empty project

---
+++ src/main/java/it/eg/sloth/Prova.java
@@ -0,0 +1,4 @@
+package it.eg.sloth;
+
+public class Prova {
+}
---
+++ src/test/java/it/eg/sloth/ProvaTest.java
@@ -0,0 +1,12 @@
+package it.eg.sloth;
+
+
+import org.junit.jupiter.api.Test;
+
+public class ProvaTest {
+
+    @Test
+    void aaa() {
+        System.out.println("rrr");
+    }
+}
